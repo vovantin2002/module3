4d2076fb084a7a6bff5d6e7375a1d2113a3da270
võ văn tín <vovantin9502@gmail.com>
2023-06-19 10:35:17 +0700

    c

--- bai_2/bai_1/src/main/java/com/example/bai_1/controller/SandwichController.java
+++ bai_2/bai_1/src/main/java/com/example/bai_1/controller/SandwichController.java
@@ -3,12 +3,9 @@ package com.example.bai_1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 
 @Controller
--- bai_2/bai_2/src/main/java/com/example/bai_2/service/CaculatorService.java
+++ bai_2/bai_2/src/main/java/com/example/bai_2/service/CaculatorService.java
@@ -7,20 +7,26 @@ import org.springframework.ui.Model;
 public class CaculatorService implements ICaculatorService {
     @Override
     public void caculator(int num1, int num2, String button, Model model) {
-        if ("add".equals(button)) {
-            model.addAttribute("result", "Addition: " + (num1 + num2));
-        } else if (button.equals("sub")) {
-            model.addAttribute("result", "Subtraction: " + (num1 - num2));
-        } else if (button.equals("mul")) {
-            model.addAttribute("result", "Multiplication: " + (num1 * num2));
-        } else if (button.equals("div")) {
-            if (num1==0||num2==0){
-                model.addAttribute("result", "Khong the chia cho 0! ");
-            }else {
-                model.addAttribute("result", "Division: " + (num1 / num2));
-            }
-        } else {
-            model.addAttribute("result", "Vui long nhap lai.  ");
+        switch (button) {
+            case "add":
+                model.addAttribute("result", "Addition: " + (num1 + num2));
+                break;
+            case "sub":
+                model.addAttribute("result", "Subtraction: " + (num1 - num2));
+                break;
+            case "mul":
+                model.addAttribute("result", "Multiplication: " + (num1 * num2));
+                break;
+            case "div":
+                if (num1 == 0 || num2 == 0) {
+                    model.addAttribute("result", "Khong the chia cho 0! ");
+                } else {
+                    model.addAttribute("result", "Division: " + (num1 / num2));
+                }
+                break;
+            default:
+                model.addAttribute("result", "Vui long nhap lai.  ");
+                break;
         }
     }
 }
