6270aa280b5719532a99e2f72dfda02686cbfa9d
võ văn tín <vovantin9502@gmail.com>
2023-06-19 10:42:01 +0700

    c

--- bai_1/bai_2/src/main/java/com/example/bai_2/repository/DictionaryRepository.java
+++ bai_1/bai_2/src/main/java/com/example/bai_2/repository/DictionaryRepository.java
@@ -8,14 +8,16 @@ import java.util.Map;
 @Repository
 public class DictionaryRepository implements IDictionaryRepository {
     private static Map<String, String> dictionaryMap = new LinkedHashMap<>();
-    @Override
-    public Map<String, String> getDictionaryMap() {
+    static {
         dictionaryMap.put("dog", "chó");
         dictionaryMap.put("cat", "mèo");
         dictionaryMap.put("mouse", "chuột");
         dictionaryMap.put("fish", "cá");
         dictionaryMap.put("duck", "vịt");
         dictionaryMap.put("chicken", "gà");
+    }
+    @Override
+    public Map<String, String> getDictionaryMap() {
         return dictionaryMap;
     }
 }
