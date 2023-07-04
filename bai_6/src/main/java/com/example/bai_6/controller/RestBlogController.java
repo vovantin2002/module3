0965b52003bc0872ef028ddbf12dcb987e51a154
võ văn tín <vovantin9502@gmail.com>
2023-07-04 10:44:03 +0700

    c

--- bai_6/src/main/java/com/example/bai_6/controller/RestBlogController.java
+++ bai_6/src/main/java/com/example/bai_6/controller/RestBlogController.java
@@ -23,44 +23,42 @@ public class RestBlogController {
 
     @GetMapping()
     public ResponseEntity<List<Blog>> display() {
-        return new ResponseEntity<>(blogService.displayList(), HttpStatus.OK) ;
+        return new ResponseEntity<>(blogService.displayList(), HttpStatus.OK);
     }
+
     @PostMapping("/search/{id}")
-    public ResponseEntity<List<Blog>> searchArticles(@PathVariable int id, Model model ) {
-        List<Blog> blogList = null;
-        try{
-            blogList = blogService.findBlogByCategoryId(id);
+    public ResponseEntity<List<Blog>> searchArticles(@PathVariable int id, Model model) {
+        List<Blog> blogList = blogService.findBlogByCategoryId(id);
+        if (blogList == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
             model.addAttribute("blogList", blogList);
-        }catch (Exception e){
-            System.out.println("id khong ton tai");
+            return new ResponseEntity<>(blogList, HttpStatus.OK);
         }
-        return new ResponseEntity<>(blogList,HttpStatus.OK);
     }
 
     @GetMapping("/category")
     public ResponseEntity<List<Category>> displayCategory() {
-        return new ResponseEntity<>(categoryService.getCategory(),HttpStatus.OK);
+        return new ResponseEntity<>(categoryService.getCategory(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Blog> showFormDetail(@PathVariable int id) {
-        Blog blog = null;
-        try{
-            blog=blogService.showBlogUpdate(id);
-        }catch (RuntimeException runtimeException){
-            System.out.println("id khong ton tai");
+        Blog blog = blogService.showBlogUpdate(id);
+        if (blog == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(blog, HttpStatus.OK);
         }
-        return new ResponseEntity<>(blog,HttpStatus.OK);
     }
 
     @GetMapping("/search/{id}")
     public ResponseEntity<List<Blog>> getPostsByCategoryId(@PathVariable int id) {
-        List<Blog> blogList = null;
-        try{
-           blogList = blogService.findBlogByCategoryId(id);
-        }catch (RuntimeException runtimeException){
-            System.out.println("id khong ton tai");
+        List<Blog> blogList = blogService.findBlogByCategoryId(id);
+        if (blogList == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(blogList, HttpStatus.OK);
         }
-        return new ResponseEntity<>(blogList,HttpStatus.OK);
     }
 }
